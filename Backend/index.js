d9871e8f3933a05e302bd549a23ece4a1e9180a5
Umesh singh <umeshsinghmehta4@gmail.com>
2025-07-25 15:08:25 +0530

    first commit

---
+++ Backend/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const dbConnect = require('./Database/Models/dbConfig');
+const app = express();
+require('dotenv').config();
+
+app.use(express.json());
+
+
+//database connection
+dbConnect();
+
+app.get('/',(req,res)=>{
+    res.json({
+        message: 'Backend is running'
+    });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
