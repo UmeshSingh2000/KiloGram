57a492ab6db6327b756eaa723c1e9414ea342ef5
Umesh singh <umeshsinghmehta4@gmail.com>
2025-07-26 14:14:48 +0530

    feat: integrate Tailwind CSS and set up routing
    - Added Tailwind CSS and its Vite plugin to the project.
    - Removed App.css and App.jsx as they were no longer needed.
    - Updated index.css to use Tailwind CSS.
    - Set up routing with react-router-dom, creating Login and Register pages.
    - Updated main.jsx to render the new routes.

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Login from './Pages/Login.jsx'
+import Register from './Pages/Register.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
+  </BrowserRouter>
 )
